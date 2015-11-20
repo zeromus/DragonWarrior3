868ac84280d3faeb7b3447559d3889ef35ee54aa
CaH4e3 <CaH4e3@a1ec70d1-dd6d-6b48-9845-34ccb8b4238d>
2015-11-20 02:29:18 +0000

    151118 finished total indexation

--- disassembly/backup.bat
+++ disassembly/backup.bat
@@ -5,7 +5,7 @@ rem copy /b notes.txt .\backup\current\notes.txt
 copy /b backup.bat .\backup\current\backup.bat
 copy /b *.inc .\backup\current\*.inc
 copy /b make.bat .\backup\current\make.bat
-copy /b dasm.exe .\backup\current\dasm.exe
+copy /b *.exe .\backup\current\*.exe
 copy /b !dd.chr .\backup\current\!dd.chr
 copy /b *.nas .\backup\current\*.nas
 copy /b copyrights.txt .\backup\current\copyrights.txt
