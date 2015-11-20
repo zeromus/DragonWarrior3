f583a29c11c72a3192a5f9751feca7cfe8ddae53
CaH4e3 <CaH4e3@a1ec70d1-dd6d-6b48-9845-34ccb8b4238d>
2015-11-20 02:16:45 +0000

    151101_1 initial movable orig source

---
+++ disassembly/bank1A.nas
@@ -0,0 +1,103 @@
+
+		PROCESSOR 6502
+
+		INCLUDE "macros.inc"
+
+		SEG.u RAM
+		INCLUDE "ram.inc"
+
+		SEG.u BANK0
+		INCLUDE "bank00.inc"
+
+		SEG.u BANK1
+		INCLUDE "bank01.inc"
+
+		SEG.u BANK2
+		INCLUDE "bank02.inc"
+
+		SEG.u BANK3
+		INCLUDE "bank03.inc"
+
+		SEG.u BANK4
+		INCLUDE "bank04.inc"
+
+		SEG.u BANK5
+		INCLUDE "bank05.inc"
+
+		SEG.u BANK6
+		INCLUDE "bank06.inc"
+
+		SEG.u BANK7
+		INCLUDE "bank07.inc"
+
+		SEG.u BANK8
+		INCLUDE "bank08.inc"
+
+		SEG.u BANK9
+		INCLUDE "bank09.inc"
+
+		SEG.u BANKA
+		INCLUDE "bank0A.inc"
+
+		SEG.u BANKB
+		INCLUDE "bank0B.inc"
+
+		SEG.u BANKC
+		INCLUDE "bank0C.inc"
+
+		SEG.u BANKD
+		INCLUDE "bank0D.inc"
+
+		SEG.u BANKE
+		INCLUDE "bank0E.inc"
+
+		SEG.u BANKF
+		INCLUDE "bank0F.inc"
+
+		SEG.u BANK10
+		INCLUDE "bank10.inc"
+
+		SEG.u BANK11
+		INCLUDE "bank11.inc"
+
+		SEG.u BANK12
+		INCLUDE "bank12.inc"
+
+		SEG.u BANK13
+		INCLUDE "bank13.inc"
+
+		SEG.u BANK14
+		INCLUDE "bank14.inc"
+
+		SEG.u BANK15
+		INCLUDE "bank15.inc"
+
+		SEG.u BANK16
+		INCLUDE "bank16.inc"
+
+		SEG.u BANK17
+		INCLUDE "bank17.inc"
+
+		SEG.u BANK18
+		INCLUDE "bank18.inc"
+
+		SEG.u BANK19
+		INCLUDE "bank19.inc"
+
+		SEG   BANK1A
+		INCLUDE "bank1A.inc"
+
+		SEG.u BANK1B
+		INCLUDE "bank1B.inc"
+
+		SEG.u BANK1C
+		INCLUDE "bank1C.inc"
+
+		SEG.u BANK1D
+		INCLUDE "bank1D.inc"
+
+		SEG.u BANK1E
+		INCLUDE "bank1E.inc"
+
+		SEG.u BANK1F
+		INCLUDE "bank1F.inc"
---
+++ disassembly/make.bat
@@ -0,0 +1,51 @@
+@echo off
+
+if "%computername%" == "HELL" (
+  rem start "" "c:\Program Files\EmEditor\EmEditor.exe" bank00.inc bank01.inc bank02.inc bank03.inc bank04.inc bank05.inc bank06.inc bank07.inc bank08.inc bank09.inc bank0A.inc bank0B.inc bank0C.inc bank0D.inc bank0E.inc bank0F.inc ram.inc bank10.inc bank11.inc bank12.inc bank13.inc bank14.inc bank15.inc bank16.inc bank17.inc bank18.inc bank19.inc bank1A.inc bank1B.inc bank1C.inc bank1D.inc bank1E.inc bank1F.inc system_bank0F_only.inc system_footer.inc system_shared.inc
+) else (
+  start "" "..\..\..\tools\TextPad 5\TextPad.exe" bank00.inc bank01.inc bank02.inc bank03.inc bank04.inc bank05.inc bank06.inc bank07.inc bank08.inc bank09.inc bank0A.inc bank0B.inc bank0C.inc bank0D.inc bank0E.inc bank0F.inc ram.inc bank10.inc bank11.inc bank12.inc bank13.inc bank14.inc bank15.inc bank16.inc bank17.inc bank18.inc bank19.inc bank1A.inc bank1B.inc bank1C.inc bank1D.inc bank1E.inc bank1F.inc system_bank0F_only.inc system_footer.inc system_shared.inc
+) 
+
+pause 0
+
+:again
+
+del !err.log
+del !dw3.nes
+del !dw3.hdr
+
+echo start.
+
+dasm system_shared.nas -f3 -osystem_shared.bin
+dasm system_footer.nas -f3 -l!dw3.lst -osystem_footer.bin >> !err.log
+
+echo assemble...
+
+for %%f in (bank*.nas) do call :dodasm %%f > NUL
+
+dasm hdr.nas -f3 -o!dw3.hdr > NUL
+
+goto :cleanup
+
+:dodasm
+dasm %1 -f3 -o%~n1.bin
+goto :eof
+
+:cleanup
+echo build...
+copy /b bank00.bin+bank01.bin+bank02.bin+bank03.bin+bank04.bin+bank05.bin+bank06.bin+bank07.bin tmp0.bin > NUL
+copy /b bank08.bin+bank09.bin+bank0A.bin+bank0B.bin+bank0C.bin+bank0D.bin+bank0E.bin+bank0F.bin tmp1.bin > NUL
+copy /b bank10.bin+bank11.bin+bank12.bin+bank13.bin+bank14.bin+bank15.bin+bank16.bin+bank17.bin tmp2.bin > NUL
+copy /b bank18.bin+bank19.bin+bank1A.bin+bank1B.bin+bank1C.bin+bank1D.bin+bank1E.bin+bank1F.bin tmp3.bin > NUL
+copy /b tmp0.bin+tmp1.bin+tmp2.bin+tmp3.bin !dw3.prg > NUL
+nesimage j !dw3 > NUL
+
+echo cleanup...
+for %%f in (*.bin) do del %%f
+
+echo done.
+pause 0
+
+goto :again
+
+:eof
