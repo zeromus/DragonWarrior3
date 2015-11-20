b1a6e99172d12ed59748086de560959877e6144b
CaH4e3 <cah4e3@users.sf.net>
2015-11-20 02:16:45 +0000

    151101_1 initial movable orig source

---
+++ disassembly/backup.bat
@@ -0,0 +1,12 @@
+mkdir .\backup\current
+copy /b
+
+rem copy /b notes.txt .\backup\current\notes.txt
+copy /b backup.bat .\backup\current\backup.bat
+copy /b *.inc .\backup\current\*.inc
+copy /b make.bat .\backup\current\make.bat
+copy /b dasm.exe .\backup\current\dasm.exe
+copy /b !dd.chr .\backup\current\!dd.chr
+copy /b *.nas .\backup\current\*.nas
+copy /b copyrights.txt .\backup\current\copyrights.txt
+copy /b notes.txt .\backup\current\notes.txt
