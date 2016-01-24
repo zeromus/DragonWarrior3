207897c9c27c940efdf7781c9a241a71fff12e30
CaH4e3 <cah4e3@users.sf.net>
2016-01-24 15:07:25 +0000

    fixed crash on failed attempt to flee and on ironize remove effect as well

--- disassembly/backup.bat
+++ disassembly/backup.bat
@@ -1,12 +1,10 @@
 mkdir .\backup\current
-copy /b
 
-rem copy /b notes.txt .\backup\current\notes.txt
+copy /b make.bat .\backup\current\make.bat
 copy /b backup.bat .\backup\current\backup.bat
 copy /b *.inc .\backup\current\*.inc
-copy /b make.bat .\backup\current\make.bat
 copy /b *.exe .\backup\current\*.exe
-copy /b !dd.chr .\backup\current\!dd.chr
 copy /b *.nas .\backup\current\*.nas
+copy /b !*.chr .\backup\current\!*.chr
 copy /b copyrights.txt .\backup\current\copyrights.txt
 copy /b notes.txt .\backup\current\notes.txt
